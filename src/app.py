e0d455025e4857d13d7a2d08209906239ff5ed24
Mohan kumar BN <mkbn02@outlook.com>
2025-08-13 20:24:52 +0530

    feat(backstage): Added CD stage

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname()
-        'owner': "mkbn-tech"
+        'owner': "mkbn tech"
     })
 
 @app.route('/api/v1/healthz')
