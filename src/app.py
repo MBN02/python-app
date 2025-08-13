f4dd13338739c5973ba52f116f949cbee2274f1a
Mohan kumar BN <mkbn02@outlook.com>
2025-08-13 17:27:27 +0530

    feat(backstage): Aded owner info in the app

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname()
+        'owner': "mkbn"
     })
 
 @app.route('/api/v1/healthz')
